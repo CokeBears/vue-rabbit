5bbe33fbf7b7243d868eb5128f2e2003fb6ccfab
Cupcake <2928494031@qq.com>
2023-08-21 14:56:01 +0800

    layout静态模板结构搭建

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView/>
-  <div class="test">
-    test scss
-  </div>
 </template>
 
 <style scoped lang="scss">
-.test{
-  color: $priceColor;
-}
 header {
   line-height: 1.5;
   max-height: 100vh;
