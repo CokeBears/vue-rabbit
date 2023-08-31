6ce48bf2b29ad5c759af96a81d03cef608a9af4a
Cupcake <2928494031@qq.com>
2023-08-31 16:56:50 +0800

    多模板适配

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -7,8 +8,10 @@
     <div class="container">
       <ul>
         <!-- 多模版渲染，区分登录状态和未登录状态 -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+
+        <!-- 登录时显示第一块 登录时有token -->
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
