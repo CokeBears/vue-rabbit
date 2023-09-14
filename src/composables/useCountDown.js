6f680132869f6ee965ca794beefcb8849622db72
Cupcake <2928494031@qq.com>
2023-09-14 19:53:53 +0800

    会员中心-个人信息页数据渲染

--- src/composables/useCountDown.js
+++ src/composables/useCountDown.js
@@ -1,8 +1,9 @@
 //封装倒计时逻辑函数
-import {computed, ref} from 'vue'
+import {computed, onUnmounted, ref} from 'vue'
 import dayjs from 'dayjs'
 export const useCountDown = () =>{
     //1.响应式数据
+    let timer = null
     const time = ref(0)
     //格式化时间 xx分xx秒
     const formatTime = computed(()=>dayjs.unix(time.value).format('mm分ss秒'))
@@ -11,10 +12,15 @@ export const useCountDown = () =>{
         //开启倒计时逻辑
         //核心逻辑编写：每秒减一
         time.value = currentTime
-        setInterval(()=>{
+        timer = setInterval(()=>{
             time.value--
         },1000)
     }
+    //组件销毁时清除定时器，防止内存泄漏
+    onUnmounted(()=>{
+        timer&&clearInterval(timer)
+    })//生命周期函数:销毁时
+
     return {
         formatTime,
         start
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,48 +25,54 @@ const router = createRouter({
       children: [
         {
           path: '',
-          name: 'home',
           component: Home
         },
         {
           path: 'category/:id',
-          name: 'category',
           component: Category
         },
         {
           path: 'category/sub/:id',
-          name: 'SubCategory',
           component: SubCategory
         },
         {
           path: 'detail/:id',
-          name: 'Detail',
           component: Detail
         },
         {
           path: 'cartlist',
-          name: 'CartList',
           component: CartList
         },
         {
           path: 'checkout',
-          name: 'Checkout',
           component: Checkout
         },
         {
           path: 'pay',
-          name: 'Pay',
           component: Pay
         },
         {
           path: 'paycallback', // 注意路径，必须是paycallback
           component: PayBack
+        },
+        {
+          path: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
         }
       ]
     },
     {
       path: '/Login',
-      name: 'login',
       component: Login
     },
   ],
