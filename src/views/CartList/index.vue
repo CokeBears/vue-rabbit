a5bf7584b8cf4361bde7c80f3d0399e02793003c
Cupcake <2928494031@qq.com>
2023-10-28 15:52:06 +0800

    提交所有代码

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -54,7 +54,7 @@ const allCheck = (selected) => {
                 <p>&yen;{{ i.price }}</p>
               </td>
               <td class="tc">
-                <el-input-number v-model="i.count" :min="1"/>
+                <el-input-number v-model="i.count" :min="1" />
               </td>
               <td class="tc">
                 <p class="f16 red">&yen;{{ (i.price * i.count).toFixed(2) }}</p>
